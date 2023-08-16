0ae5ef4f1ea3e067486580e98fea65bb5508931d
samimm11 <sam.imm11@gmail.com>
2023-08-16 20:30:53 +0530

    Created new Project

---
+++ reduxdemo/src/App.js
@@ -0,0 +1,14 @@
+import Login from "./Login";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Redux</h1>
+      <Login></Login>
+      <Profile></Profile>
+    </div>
+  );
+}
+
+export default App;
